db91a4ee41a4f27e9e7595250e07b0f90b2e64c9
DhiaOmri <zawachiomri@gmail.com>
2021-09-17 21:12:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
+import Logo from "./img/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./App.css";
+class App extends React.Component {
+  state = {
+    fullName: "Dhia Omri",
+    bio: "",
+    imgSrc: Logo,
+    profession: "",
+    shows: false,
+  };
+  handleClick = () => this.setState({ shows: "true" });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+  alert('component Did Mount')
+  }
+
+  componentWillUnmount() {
+   alert('component Will Unmount')
+  }
+  componentDidUpdate(){
+    alert('component Did Update')
+  }
+  render() { 
+    
+    return (  
+      <div className="App">
+        <div className="content">
+        <img className="logo" src={this.state.shows?this.state.imgSrc:""} alt="" />
+        <h1>{this.state.shows?this.state.fullName:""}</h1>
+        <p>{this.state.shows?this.state.bio:""}</p>
+        <p>{this.state.shows?this.state.profession:""}</p>
+      {this.state.shows? ( <ul className="social-link">
+          <a href="https://github.com/DhiaOmri" className="social">
+            <FontAwesomeIcon icon={faGithub} style={{ fontSize: "30px" }} />
+          </a>
+          <a href="https://github.com/DhiaOmri">
+            <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: "30px" }} />
+          </a>
+        </ul>):""}
+        <button onClick={this.handleClick} className="app-button"> ClickMe</button>
+       
+
+        </div>
+    
+      </div>
+    );
+  }
 }
 
 export default App;
